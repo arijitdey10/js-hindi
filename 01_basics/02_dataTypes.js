4942bffb1a5dad3a3b1663a5585af34fcdf244d9
arijitdey10 <114436163+arijitdey10@users.noreply.github.com>
2023-09-27 19:40:08 +0000

    .

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict" //treat all js code as newer version or standerd
+
+//alert("gogo") // we cant use this becuse we are using nodejs, not browser
+
+console.log(3 + 
+    2)
+    /* we can't write it like this 
+        code readability is most importent thing in javascript */
+
+//number => 2 to power 53
+//bigint
+//string => "...."
+//boolean => true/false
+//null => stand alone value
+//undefined
+//symbol => unique
+
+//object
+
+console.log(typeof undefined);
+console.log(typeof null);
