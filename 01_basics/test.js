261ebdcf9cc6aa55aff7348e23fc7584fe058e6f
arijitdey10 <114436163+arijitdey10@users.noreply.github.com>
2023-10-01 14:11:27 +0000

    .

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,38 @@
-console.log("dhu")
+"use strict"
+let obj1 ={
+    name : "superman",
+    age : 20,
+    id : {
+        1 : "abc.1",
+        2 : "abc.2",
+        3 : [1,2,3]
+    }
+}
+
+let obj2 ={
+    name : "batman",
+    age : 21,
+    id : {
+        1 : "ABC.1",
+        2 : "ABC.2",
+        3 : [4,5,6]
+    }
+}
+
+let obj = Object.assign(obj1)
+console.log(obj1);
+
+
+
+
+// new Set()	
+// add()	
+// delete()	
+// has()	
+// clear()	
+// forEach()	
+// values()	
+// keys()	
+// entries()	
+
+// size
